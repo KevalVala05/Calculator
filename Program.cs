266416a301f3e5b8711fe276e2cdfe5e63b5ff0b
Keval Vala <keval.vala.05.08@gmail.com>
2023-02-09 20:56:25 -0800

    second commit

---
+++ Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basicCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the first number :");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter the Operator :");
+            string operand = Console.ReadLine();
+
+            Console.WriteLine("Enter the second number :");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+
+            if (operand == "+")
+            {
+                Console.WriteLine(num1 + num2);
+            }
+
+            else if (operand == "-")
+            {
+                Console.WriteLine(num1 - num2);
+            }
+
+            else if(operand == "*")
+            {
+                Console.WriteLine(num1 * num2);
+            }
+
+            else if(operand == "/")
+            {
+                Console.WriteLine(num1 / num2);
+            }
+
+            else
+            {
+                Console.WriteLine("Sorry, you entered a wrong operator !");
+            }
+
+            Console.ReadLine(); 
+        }
+    }
+}
